56fabbb3862df296f78664a78ff4607590290e29
marcelo <hemakesnoise@gmail.com>
2013-10-04 23:37:53 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Educore::Application.config.secret_token = '0f18ed7b7770e301c2daddd1dea6e04ebc3a772d96d0b70d26fc1cc24c1016ad9d9feb6300ece0b4ac20de362ead4f823c350acf7564af6ec086c5ca00e51ddd'
